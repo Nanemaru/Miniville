994ee40f2d94454c24d1e66f6949e728f31adf6d
Nanemaru <mylanboitel@gmail.com>
2025-11-13 16:20:00 +0100

    Ajout classe CardsInfo et modif Program
    Utilisation des codes données dans le cours

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MiniVille
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game g = new Game();
+        }
+    }
+}
