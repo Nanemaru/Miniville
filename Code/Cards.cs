5719f95dc67fea0f2124e5947b9eb39faa460979
Nanemaru <mylanboitel@gmail.com>
2025-11-17 16:39:46 +0100

    Modif cards

--- Code/Cards.cs
+++ Code/Cards.cs
@@ -1,127 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace MiniVille
 {
-        internal class Cards
+    public class Cards
     {
-        public int _id;
-        public string _color;
-        public int _cost;
-        public string _name;
-        public string _effect;
-        public int _dice;
-        public int _gain;
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Color { get; private set; }
+        public int Cost { get; private set; }
+        public string Effect { get; private set; }
+        public int Dice { get; private set; }
+        public int Gain { get; private set; }
 
-        /// Dictionnaire pour associer les id et les noms des cartes <Key, Value>
-        public Dictionary<int, string> cardsDictionnary = new Dictionary<int, string>()
+        public Cards(CardsInfo info)
         {
-            {1, "Champs de blé" } , {2, "Ferme"}, {3, "Boulangerie"}, {4, "Café"} ,
-            {5, "Superette"}, {6, "Forêt"}, {7, "Restaurant"}, {8, "Stade"}
-        };
-
-        internal Cards(CardsInfo info)
-        {
-            _id = info.Id;
-            _color = info.Color;
-            _cost = info.Cost;
-            _name = info.Name;
-            _effect = info.Effect;
-            _dice = info.Dice;
-            _gain = info.Gain; 
+            Id = info.Id;
+            Name = info.Name;
+            Color = info.Color;
+            Cost = info.Cost;
+            Effect = info.Effect;
+            Dice = info.Dice;
+            Gain = info.Gain;
         }
-
-        public void SetInfos(int id)
-        {
-            switch (id)
-            {
-                case 1: //Champ de blé
-                    _dice = 1;
-                    _color = "blue";
-                    _name = cardsDictionnary[id];
-                    _effect = "Recevez 1 pièce";
-                    _gain = 1;
-                    _cost = 1;
-                    break;
-
-                case 2: //Ferme
-                    _dice = 1;
-                    _color = "blue";
-                    _name = cardsDictionnary[id];
-                    _effect = "Recevez 1 pièce";
-                    _gain = 1;
-                    _cost = 2;
-                    break;
-
-                case 3: //Boulangerie
-                    _dice = 2;
-                    _color = "green";
-                    _name = cardsDictionnary[id];
-                    _effect = "Recevez 2 pièce";
-                    _gain = 2;
-                    _cost = 1;
-                    break;
-
-                case 4: //Café
-                    _dice = 3;
-                    _color = "red";
-                    _name = cardsDictionnary[id];
-                    _effect = "Recevez 1 pièce du joueur qui a lancé le dé";
-                    _gain = 2;
-                    _cost = 2;
-                    break;
-
-                case 5: //Superette
-                    _dice = 4;
-                    _color = "green";
-                    _name = cardsDictionnary[id];
-                    _effect = "Recevez 3 pièces";
-                    _gain = 3;
-                    _cost = 2;
-                    break;
-
-                case 6: //Forêt
-                    _dice = 5;
-                    _color = "blue";
-                    _name = cardsDictionnary[id];
-                    _effect = "Recevez 1 pièce ";
-                    _gain = 1;
-                    _cost = 2;
-                    break;
-
-                case 7: //Restaurant
-                    _dice = 5;
-                    _color = "red";
-                    _name = cardsDictionnary[id];
-                    _effect = "Recevez 2 pièces du joueur qui a lancé le dé ";
-                    _gain = 2;
-                    _cost = 4;
-                    break;
-
-                case 8: //Stade
-                    _dice = 6;
-                    _color = "blue";
-                    _name = cardsDictionnary[id];
-                    _effect = "Recevez 4 pièce";
-                    _gain = 4;
-                    _cost = 6;
-                    break;
-            }
-        }
-
 
         public override string ToString()
         {
-            string cardInfos = String.Format("Carte tirée : {0}\n", this._name);
-            cardInfos += String.Format("Couleur : {0}\n", this._color);
-            cardInfos += String.Format("Effet : {0}\n, GAIN : {1}\n", this._effect, this._gain);
-            cardInfos += String.Format("Prix : {0}\n", this._cost);
-            cardInfos += String.Format("Valeur activation : {0}\n", this._dice);
-
-            return cardInfos;
+            return $"[{Dice}] {Color} - {Name} : {Effect} - {Cost}$";
         }
     }
 }
