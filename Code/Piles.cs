339286819d2aab59a751a27a3d45b9fa979b6022
Thomas OLIVON <tolivon07@gmail.com>
2025-11-17 16:05:51 +0100

    Update Card to Cards

--- Code/Piles.cs
+++ Code/Piles.cs
@@ -8,18 +8,18 @@ namespace Miniville
 {
     public class Piles
     {
-        public List<Card> AvailableCards = new List<Cards> { };
+        public List<Cards> AvailableCards = new List<Cards> { };
 
-        private Card champBle = new Card(0, "Bleu", 1, "Champs de blé", "Recevez 1 pièce", 1, 1);
-        private Card ferme = new Card(1, "Bleu", 2, "Ferme", "Recevez 1 pièce", 1, 1);
-        private Card boulangerie = new Card(2, "Vert", 1, "Boulangerie", "Recevez 2 pièces", 2, 2);
-        private Card cafe = new Card(3, "Rouge ", 2, "Café", "Recevez 1 pièce du joueur qui a lancé le dé", 3, 1);
-        private Card superette = new Card(4, "Vert ", 2, "Superette", "Recevez 3 pièces", 4, 3);
-        private Card foret = new Card(5, "Bleu ", 2, "Forêt", "Recevez 1 pièce", 5, 1);
-        private Card restaurant = new Card(6, "Rouge ", 4, "Restaurant", "Recevez 2 pièces du joueur qui a lancé le dé", 5, 2);
-        private Card stade = new Card(7, "Bleu ", 6, "Stade", "Recevez 4 pièces", 6, 4);
+        private Cards champBle = new Cards(0, "Bleu", 1, "Champs de blé", "Recevez 1 pièce", 1, 1);
+        private Cards ferme = new Cards(1, "Bleu", 2, "Ferme", "Recevez 1 pièce", 1, 1);
+        private Cards boulangerie = new Cards(2, "Vert", 1, "Boulangerie", "Recevez 2 pièces", 2, 2);
+        private Cards cafe = new Cards(3, "Rouge ", 2, "Café", "Recevez 1 pièce du joueur qui a lancé le dé", 3, 1);
+        private Cards superette = new Cards(4, "Vert ", 2, "Superette", "Recevez 3 pièces", 4, 3);
+        private Cards foret = new Cards(5, "Bleu ", 2, "Forêt", "Recevez 1 pièce", 5, 1);
+        private Cards restaurant = new Cards(6, "Rouge ", 4, "Restaurant", "Recevez 2 pièces du joueur qui a lancé le dé", 5, 2);
+        private Cards stade = new Cards(7, "Bleu ", 6, "Stade", "Recevez 4 pièces", 6, 4);
 
-        Dictionary<int, Card> dico = new Dictionary<int, Card>
+        Dictionary<int, Cards> dico = new Dictionary<int, Cards>
         {
             {0, champBle},
             {1, ferme},
@@ -29,10 +29,10 @@ namespace Miniville
             {5, foret},
             {6, restaurant},
             {7, stade},
-        } 
+        };
 
 
-        public Piles(/* List<Card> availableCards */)
+        public Piles(/* List<Cards> availableCards */)
         {
             // AvailableCards = availableCards;
             InitializeCards();
@@ -52,7 +52,7 @@ namespace Miniville
                 AvailableCards.Add(restaurant);
                 AvailableCards.Add(stade);
                 */
-                for(int j = 0; j < 8; j++) AvailableCards.Add(dico[j].value);
+                for (int j = 0; j < 8; j++) AvailableCards.Add(dico[j].Value);
             }
         }
 
@@ -62,18 +62,18 @@ namespace Miniville
             for (int i = 0; i < 8; i++)
             {
                 int cardCount = 0;
-                foreach (var card in AvailableCards)
+                foreach (var Cards in AvailableCards)
                 {
-                    if (card.id == i)
+                    if (Cards.Id == i)
                     {
                         cardCount++;
                     }
                 }
 
-                toString += string.Format("{0} * {1} - {2} [{3}] : {4} - {5}$ \n", cardCount, dico[i].Name, dico[i].Color, dico[i].Dice, dico[i].Effect, dico[i].Cost)
-
-                return toString;
+                toString += string.Format("{0} * {1} - {2} [{3}] : {4} - {5}$ \n", cardCount, dico[i].Name, dico[i].Color, dico[i].Dice, dico[i].Effect, dico[i].Cost);
             }
+
+            return toString;
         }
     }
 }
