eec516260a968172f86e3339b4c43452e552de58
mistail1510 <mistail1510@gmail.com>
2025-11-17 13:31:55 +0100

    Création Dice

--- Code/Dice.cs
+++ Code/Dice.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Dice
+    public class Dice
     {
+        private Random rdm = new Random();
+
+        public static Dice() { }
+
+        public static int Roll()
+        {
+            int result = rdm.Next(1, 7);
+            return result;
+        }
+
     }
 }
