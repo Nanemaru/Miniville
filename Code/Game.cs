d09c7acd97ce17a94aad18fba66f105cc5ea25a3
Kerian <kerian.gouriou@gmail.com>
2025-11-18 10:31:16 +0100

    Modif Game and Piles

--- Code/Game.cs
+++ Code/Game.cs
@@ -31,9 +31,10 @@ namespace MiniVille
         {
             Console.WriteLine("=== MiniVilles ===");
             Console.WriteLine("Objectif : atteindre 20 pièces pour gagner !");
-
+            //Boucle de gameplay
             while (true)
             {
+                //Si la fonction PlayerTurn retourne true dans un des deux if, la partie prend fin
                 if (PlayerTurn(player, ai)) break;
                 if (PlayerTurn(ai, player, isAI: true)) break;
             }
@@ -49,6 +50,7 @@ namespace MiniVille
                 Console.ReadLine();
             }
 
+            //On lance le dé
             int result = dice.Roll();
 
             // Activation des cartes
@@ -77,6 +79,12 @@ namespace MiniVille
 
         private void PlayerBuyCard(Player player, int result)
         {
+            /*
+            Affichage dans un ordre précis pour des questionss de lisibilité
+            - Résultat du dé
+            - Nombre de pièces
+            - Question au joueur
+            */
             Console.WriteLine($"{player.Name} a obtenu {result}.");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Pièces : {0}", player.Money);
@@ -84,40 +92,56 @@ namespace MiniVille
             Console.WriteLine("Souhaitez-vous acheter une carte ? (o/n)");
             string choice = Console.ReadLine()?.ToLower();
 
-            if (choice == "o")
-            {
-                List<int> alreadyDisplayedCards = new List<int>();
-                int cardCount = 0;
-                foreach (Cards c in piles.AvailableCards)
+                switch (choice)
                 {
-                    if (!alreadyDisplayedCards.Contains(c.Id))
+                case "o" | "oui" | "Oui":
                     {
-                        alreadyDisplayedCards.Add(c.Id);
-                        for (int i = 0; i < piles.AvailableCards.Count; i++)
+                        List<int> alreadyDisplayedCards = new List<int>();
+                        int cardCount = 0;
+                        foreach (Cards c in piles.AvailableCards)
                         {
-                            if (piles.AvailableCards[i].Id == c.Id)
+                            if (!alreadyDisplayedCards.Contains(c.Id))
                             {
-                                cardCount++;
+                                alreadyDisplayedCards.Add(c.Id);
+                                for (int i = 0; i < piles.AvailableCards.Count; i++)
+                                {
+                                    if (piles.AvailableCards[i].Id == c.Id)
+                                    {
+                                        cardCount++;
+                                    }
+                                }
+                                Console.WriteLine("x{0} {1}", cardCount, c.Name);
+                                cardCount = 0;
                             }
                         }
-                        Console.WriteLine("x{0} {1}", cardCount, c.Name);
-                        cardCount = 0;
-                    }
-                }
-                Console.WriteLine("Entrez le nom exact de la carte à acheter :");
-                string name = Console.ReadLine();
+                        Console.WriteLine("Entrez le nom exact de la carte à acheter :");
+                        string name = Console.ReadLine();
 
-                var card = piles.AvailableCards.FirstOrDefault(c => c.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-                if (card != null && player.Money >= card.Cost)
-                {
-                    player.AddCard(card);
-                    piles.AvailableCards.Remove(card);
-                    Console.WriteLine($"{player.Name} achète {card.Name} !"); //good
-                }
-                else
-                {
-                    Console.WriteLine("Achat impossible.");
-                }
+
+                        var card = piles.AvailableCards.FirstOrDefault(c => c.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+                        if (card != null && player.Money >= card.Cost)
+                        {
+                            player.AddCard(card);
+                            piles.AvailableCards.Remove(card);
+                            Console.WriteLine($"{player.Name} achète {card.Name} !"); //good
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Achat impossible.");
+                            break;
+                        }
+                    }
+                case "n" | "non" | "Non":
+                    {
+                        Console.WriteLine("{0} n'achète rien", player.Name);
+                    }
+                default:
+                    {
+                        Console.WriteLine("Vous devez choisir une réponse valide.");
+                        Continue;
+                    }
+            }
             }
         }
 
--- Code/Piles.cs
+++ Code/Piles.cs
@@ -5,7 +5,8 @@ namespace MiniVille
     public class Piles
     {
         public List<Cards> AvailableCards = new List<Cards> { };
-
+        //On fait appel au struct CardsInfo via la classe Cards dans laquelle il est contenu
+        //Les noms de certaines variables sont ici en français pour des raisons d'affichage
         private static Cards.CardsInfo champBle = new Cards.CardsInfo(0, "Blue", 1, "Champs de blé", "Recevez 1 pièce", 1, 1);
         private static Cards.CardsInfo ferme = new Cards.CardsInfo(1, "Blue", 2, "Ferme", "Recevez 1 pièce", 1, 1);
         private static Cards.CardsInfo boulangerie = new Cards.CardsInfo(2, "Green", 1, "Boulangerie", "Recevez 2 pièces", 2, 2);
@@ -15,6 +16,7 @@ namespace MiniVille
         private static Cards.CardsInfo restaurant = new Cards.CardsInfo(6, "Red", 4, "Restaurant", "Recevez 2 pièces du joueur qui a lancé le dé", 5, 2);
         private static Cards.CardsInfo stade = new Cards.CardsInfo(7, "Blue", 6, "Stade", "Recevez 4 pièces", 6, 4);
 
+        //Pourrais être remplacé par une liste
         Dictionary<int, Cards.CardsInfo> dico = new Dictionary<int, Cards.CardsInfo>
         {
             {0, champBle},
@@ -28,23 +30,25 @@ namespace MiniVille
         };
 
 
-        public Piles(/* List<Cards> availableCards */)
+        public Piles()
         {
-            // AvailableCards = availableCards;
             InitializeCards();
         }
 
         private void InitializeCards()
         {
+            //On remplie la liste AvailableCards avec un exemplaire de chaque carte 6 fois d'affilées
             for (int i = 0; i < 6; i++)
             {
                 for (int j = 0; j < 8; j++) AvailableCards.Add(new Cards(dico[j]));
             }
         }
 
+        /// La fonction ToString() sert à retourner un string lorsqu'une référence à une instance de la classe est écrite dans un emplacement où un string est attendu 
         public override string ToString()
         {
             string toString = string.Format("Cartes disponibles : \n");
+            //Pour chaque type de batiment afficher un compte du nombre d'exemplaire du batîment encore en boutique ainsi que des infos détaillés
             for (int i = 0; i < 8; i++)
             {
                 int cardCount = 0;
@@ -55,7 +59,6 @@ namespace MiniVille
                         cardCount++;
                     }
                 }
-
                 toString += string.Format("{0} * {1} - {2} [{3}] : {4} - {5}$ \n", cardCount, dico[i].Name, dico[i].Color, dico[i].Dice, dico[i].Effect, dico[i].Cost);
             }
 
