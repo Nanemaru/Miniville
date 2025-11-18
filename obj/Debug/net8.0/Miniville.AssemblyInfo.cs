bf64531452cba433318f3959da965f9ff88de0ff
mistail1510 <mistail1510@gmail.com>
2025-11-18 10:19:17 +0100

    Petit fix Solution

--- obj/Debug/net8.0/Miniville.AssemblyInfo.cs
+++ obj/Debug/net8.0/Miniville.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miniville")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb16300ba18e22a4d0ba3db2256d6434213fc306")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miniville")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miniville")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
