36b6c24ae2396aff5cbef73c9f2e270ec34c8114
Kerian <kerian.gouriou@gmail.com>
2025-11-18 15:17:51 +0100

    Re test debug git

--- obj/Debug/net8.0/Miniville.AssemblyInfo.cs
+++ obj/Debug/net8.0/Miniville.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miniville")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb91fa06f96368c144abc62534718eba8166df0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dec1bfbdf08dca7474b995dbbcb58a89de73925")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miniville")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miniville")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
